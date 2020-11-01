24badbab1d8929f5f3f9f0173347f115ed04f7c7
Guillaume Baptist <guillaume.baptist@free.fr>
2020-11-01 02:24:59 +0100

    commit cherrypick

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
+<<<<<<< HEAD
     cout << "Hello world 14!" << endl;
+=======
+    cout << "Hello world 16!" << endl;
+>>>>>>> 024b45f... commit 16
     return 0;
 }
